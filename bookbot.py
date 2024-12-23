260c4ba125c7f1eab0e0b44cc00705fb4d7a2190
Tirth Patel <tirthpatel61@outlook.com>
2024-12-23 21:10:13 +0530

    Read document specified in command line argument
    If the command line args specify a documents, read it inside the
    `print_statistics` function. We can later use the content stored in
    `content` variable as `str` to calculate statistics.

--- bookbot.py
+++ bookbot.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
+import sys
 
 
 def main():
-    print("Usage: bookbot.py [document]")
+    cmd_args = sys.argv[1:]
+    if len(cmd_args) == 1:
+        print_statistics(cmd_args[0])
+    else:
+        print("Usage: bookbot.py [document]")
+
+
+def print_statistics(filename: str):
+    content = ""
+    try:
+        with open(filename) as f:
+            content = f.read()
+    except FileNotFoundError:
+        print(f"File '{filename}' not found!")
 
 
 if __name__ == "__main__":
